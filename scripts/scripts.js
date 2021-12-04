e54d5bd2db9df82b705a506fd520687b4341cbb9
EdgarMTDev <edgarmontiel961@gmail.com>
2021-12-03 23:13:48 -0600

    AppStructure

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+ScrollReveal().reveal('.container0',  {delay: 500});
+ScrollReveal().reveal('.contenedor1', {delay:500});
+ScrollReveal().reveal('.banner-one', {delay:500});
+ScrollReveal().reveal('.banner-two', {delay:500});
+
+
+swal('Bienvenido', '', 'success');
+
+
+
+
